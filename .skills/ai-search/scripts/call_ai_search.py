Majidmum/enter.shop

Initial miaoda project setup with React TypeScript Vite template 24bcfe376599d1be4d3f212bf5d9cffa4aa58f41 no sync

.skills/ai-search/scripts/call_ai_search.py
@@ -1,0 +1,120 @@
+#!/usr/bin/env python3
+"""Call the AI Search API (Gemini 2.5 Flash + Google grounding) and aggregate the SSE response."""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.request
+
+
+ENDPOINT = "https://app-dm6bkvkknf29-api-zYm4ze3j7XvL.gateway.appmedo.com/v1beta/models/gemini-2.5-flash:streamGenerateContent?alt=sse"
+
+
+def fail(message):
+    """Print an error message to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_json(value, name):
+    """Parse a JSON string argument; exit on failure."""
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError as exc:
+        fail("Invalid JSON for %s: %s" % (name, exc))
+
+
+def iter_sse(response):
+    """Iterate over an SSE response, yielding each data field value."""
+    for raw_line in response:
+        line = raw_line.decode("utf-8", errors="replace").strip()
+        if not line or not line.startswith("data:"):
+            continue
+        data = line[5:].strip()
+        if data == "[DONE]":
+            break
+        yield data
+
+
+def main():
+    """Entry point: parse arguments, call AI Search and print aggregated JSON result."""
+    parser = argparse.ArgumentParser(description="Call AI Search (Gemini + Google grounding).")
+    parser.add_argument("--query", help="User search question. Ignored when --contents is provided.")
+    parser.add_argument("--contents", help="JSON array of conversation contents (role/parts).")
+    parser.add_argument("--timeout", type=int, default=600, help="request timeout in seconds")
+    args = parser.parse_args()
+
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    if args.contents:
+        contents = parse_json(args.contents, "--contents")
+    elif args.query:
+        contents = [{"role": "user", "parts": [{"text": args.query}]}]
+    else:
+        fail("Either --query or --contents is required")
+
+    body = json.dumps({"contents": contents}, ensure_ascii=False).encode("utf-8")
+    req = urllib.request.Request(
+        ENDPOINT,
+        data=body,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "text/event-stream",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+
+    text_parts = []
+    grounding_chunks = []
+    web_search_queries = []
+
+    try:
+        with urllib.request.urlopen(req, timeout=args.timeout) as response:
+            for data in iter_sse(response):
+                try:
+                    chunk = json.loads(data)
+                except json.JSONDecodeError:
+                    continue
+                for candidate in chunk.get("candidates", []):
+                    content = candidate.get("content", {})
+                    for part in content.get("parts", []):
+                        text = part.get("text")
+                        if text:
+                            text_parts.append(text)
+                    metadata = candidate.get("groundingMetadata")
+                    if metadata:
+                        chunks = metadata.get("groundingChunks")
+                        if isinstance(chunks, list):
+                            grounding_chunks = chunks
+                        queries = metadata.get("webSearchQueries")
+                        if isinstance(queries, list):
+                            web_search_queries = queries
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        fail("HTTP %s: %s" % (exc.code, detail))
+    except urllib.error.URLError as exc:
+        fail("Request failed: " + str(exc.reason))
+    except TimeoutError:
+        fail("Request timed out")
+
+    sources = []
+    for gc in grounding_chunks:
+        web = gc.get("web", {})
+        if web.get("uri"):
+            sources.append({"uri": web["uri"], "title": web.get("title", "")})
+
+    print(json.dumps({
+        "status": "succeed",
+        "text": "".join(text_parts),
+        "sources": sources,
+        "webSearchQueries": web_search_queries,
+    }, ensure_ascii=False, separators=(",", ":")))
+
+
+if __name__ == "__main__":
+    main()

.skills/image-to-video/scripts/query_image_to_video.py
@@ -1,0 +1,128 @@
+#!/usr/bin/env python3
+"""
+Poll an already-submitted Kling image-to-video task by task_id.
+
+Use this when generate_image_to_video.py returned {"status":"processing","task_id":"..."}.
+
+Exit codes:
+    0 - success, prints JSON:
+        {"status":"succeed","task_id":"...","videos":[{"url":"...","duration":"...","file":"/path"}]}
+        or, if not finished within the safe time limit:
+        {"status":"processing","task_id":"..."}
+    1 - API or argument error
+"""
+
+import argparse
+import json
+import os
+import sys
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+QUERY_URL_BASE = "https://app-dm6bkvkknf29-api-oYA6Z8wDBRDa.gateway.appmedo.com/v1/videos/image2video"
+POLL_INTERVAL_S = 7
+SAFE_LIMIT_S = 550
+
+
+def fail(message):
+    """Print an error message to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_args():
+    """Parse command line arguments."""
+    p = argparse.ArgumentParser(description="Poll a Kling image-to-video task.")
+    p.add_argument("--task-id", required=True, help="task ID to query")
+    p.add_argument("--output-dir", help="directory to download result videos into")
+    return p.parse_args()
+
+
+def api_key():
+    """Read the API key from the environment; exit if unset."""
+    key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not key:
+        fail("INTEGRATIONS_API_KEY is required")
+    return key
+
+
+def get_json(url, key):
+    """Send a GET request and return the parsed JSON response."""
+    req = urllib.request.Request(
+        url,
+        headers={"X-Gateway-Authorization": "Bearer " + key},
+        method="GET",
+    )
+    try:
+        with urllib.request.urlopen(req, timeout=60) as resp:
+            return json.loads(resp.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        body = e.read().decode("utf-8", errors="replace")
+        raise RuntimeError("HTTP %s: %s" % (e.code, body))
+
+
+def unwrap(data):
+    """Validate the response code and extract the data payload."""
+    if data.get("code") not in (None, 0):
+        raise RuntimeError("API error %s: %s" % (data.get("code"), data.get("message")))
+    return data.get("data", data)
+
+
+def extract_videos(data):
+    """Extract the generated video list from the task result."""
+    result = data.get("task_result") or data.get("taskResult") or {}
+    return result.get("videos") or []
+
+
+def download_videos(videos, output_dir):
+    """Download result videos into the output dir; return URLs only when no dir is given."""
+    if not output_dir:
+        return [{"url": v.get("url"), "duration": v.get("duration"), "file": None} for v in videos]
+    os.makedirs(output_dir, exist_ok=True)
+    downloaded = []
+    for idx, v in enumerate(videos):
+        url = v.get("url")
+        ext = os.path.splitext(urllib.parse.urlparse(url).path)[1] or ".mp4"
+        file_path = os.path.join(output_dir, "video_%s%s" % (idx, ext))
+        urllib.request.urlretrieve(url, file_path)
+        downloaded.append({"url": url, "duration": v.get("duration"), "file": file_path})
+    return downloaded
+
+
+def poll_task(task_id, key, output_dir):
+    """Poll the task status until it succeeds, fails, or the safe limit is reached."""
+    deadline = time.time() + SAFE_LIMIT_S
+    while time.time() < deadline:
+        time.sleep(POLL_INTERVAL_S)
+        data = unwrap(get_json(QUERY_URL_BASE + "/" + urllib.parse.quote(task_id), key))
+        status = data.get("task_status") or data.get("status")
+        if status == "succeed":
+            videos = extract_videos(data)
+            if not videos:
+                raise RuntimeError("succeed response missing videos: %s" % json.dumps(data, ensure_ascii=False))
+            print(json.dumps(
+                {"status": "succeed", "task_id": task_id, "videos": download_videos(videos, output_dir)},
+                ensure_ascii=False,
+            ))
+            return
+        if status in ("failed", "failure"):
+            msg = data.get("task_status_msg") or data.get("message") or "unknown error"
+            raise RuntimeError("Task %s failed: %s" % (task_id, msg))
+    print(json.dumps({"status": "processing", "task_id": task_id}, ensure_ascii=False))
+
+
+def main():
+    """Entry point: poll the image-to-video task and print the result JSON."""
+    args = parse_args()
+    key = api_key()
+    try:
+        poll_task(args.task_id, key, args.output_dir)
+    except Exception as e:
+        fail(str(e))
+
+
+if __name__ == "__main__":
+    main()

.skills/text-to-speech/scripts/call_text_to_speech.py
@@ -1,0 +1,102 @@
+#!/usr/bin/env python3
+"""Call the LemonFox text-to-speech API and save the resulting audio to a file."""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.request
+
+
+ENDPOINT = "https://app-dm6bkvkknf29-api-GYX1lzGw01Xa.gateway.appmedo.com/v1/audio/speech"
+
+
+def fail(message):
+    """Print an error to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def report_error_body(raw_bytes):
+    """Parse a JSON/text error body and exit with a readable message."""
+    text = raw_bytes.decode("utf-8", errors="replace")
+    try:
+        data = json.loads(text)
+    except ValueError:
+        fail("API error: " + text)
+        return
+    msg = data.get("message") or data.get("error") or json.dumps(data, ensure_ascii=False)
+    fail("API error: " + str(msg))
+
+
+def main():
+    """Entry point: synthesize text to speech and save the audio file."""
+    parser = argparse.ArgumentParser(description="Synthesize text to speech and save the audio file.")
+    parser.add_argument("--input", required=True, help="Text content to convert to speech.")
+    parser.add_argument("--output", required=True, help="Local file path to save the synthesized audio to.")
+    parser.add_argument("--voice", default="heart", help="Voice type, e.g. heart. Default: heart.")
+    parser.add_argument("--response-format", dest="response_format", default="mp3",
+                        help="Output audio format: mp3, wav, ogg. Default: mp3.")
+    parser.add_argument("--timeout", type=int, default=600, help="Request timeout in seconds. Default: 600.")
+    args = parser.parse_args()
+
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    body = {
+        "input": args.input,
+        "voice": args.voice,
+        "response_format": args.response_format,
+    }
+    request = urllib.request.Request(
+        ENDPOINT,
+        data=json.dumps(body, ensure_ascii=False).encode("utf-8"),
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+
+    try:
+        with urllib.request.urlopen(request, timeout=args.timeout) as response:
+            content_type = response.headers.get("Content-Type", "")
+            raw = response.read()
+    except urllib.error.HTTPError as exc:
+        raw = exc.read()
+        content_type = exc.headers.get("Content-Type", "") if exc.headers else ""
+        if "json" in content_type or "text" in content_type:
+            report_error_body(raw)
+        else:
+            fail("HTTP %s: %s" % (exc.code, raw.decode("utf-8", errors="replace")))
+        return
+    except urllib.error.URLError as exc:
+        fail("Request failed: " + str(exc.reason))
+        return
+    except TimeoutError:
+        fail("Request timed out")
+        return
+
+    # If the API returned JSON/text, it's an error message wrapped in a normal 200
+    if "json" in content_type or "text" in content_type:
+        report_error_body(raw)
+        return
+
+    try:
+        with open(args.output, "wb") as fh:
+            fh.write(raw)
+    except OSError as exc:
+        fail("Failed to write audio file %s: %s" % (args.output, exc))
+        return
+
+    print(json.dumps(
+        {"status": "succeed", "file": args.output, "bytes": len(raw)},
+        ensure_ascii=False,
+        separators=(",", ":"),
+    ))
+
+
+if __name__ == "__main__":
+    main()

olybless89-cyber/Prince-1st-cloud-client

.skills/ai-search/scripts/call_ai_search.py
@@ -1,0 +1,120 @@
+#!/usr/bin/env python3
+"""Call the AI Search API (Gemini 2.5 Flash + Google grounding) and aggregate the SSE response."""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.request
+
+
+ENDPOINT = "https://app-dkpgdv8fqio1-api-zYm4ze3j7XvL.gateway.appmedo.com/v1beta/models/gemini-2.5-flash:streamGenerateContent?alt=sse"
+
+
+def fail(message):
+    """Print an error message to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_json(value, name):
+    """Parse a JSON string argument; exit on failure."""
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError as exc:
+        fail("Invalid JSON for %s: %s" % (name, exc))
+
+
+def iter_sse(response):
+    """Iterate over an SSE response, yielding each data field value."""
+    for raw_line in response:
+        line = raw_line.decode("utf-8", errors="replace").strip()
+        if not line or not line.startswith("data:"):
+            continue
+        data = line[5:].strip()
+        if data == "[DONE]":
+            break
+        yield data
+
+
+def main():
+    """Entry point: parse arguments, call AI Search and print aggregated JSON result."""
+    parser = argparse.ArgumentParser(description="Call AI Search (Gemini + Google grounding).")
+    parser.add_argument("--query", help="User search question. Ignored when --contents is provided.")
+    parser.add_argument("--contents", help="JSON array of conversation contents (role/parts).")
+    parser.add_argument("--timeout", type=int, default=600, help="request timeout in seconds")
+    args = parser.parse_args()
+
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    if args.contents:
+        contents = parse_json(args.contents, "--contents")
+    elif args.query:
+        contents = [{"role": "user", "parts": [{"text": args.query}]}]
+    else:
+        fail("Either --query or --contents is required")
+
+    body = json.dumps({"contents": contents}, ensure_ascii=False).encode("utf-8")
+    req = urllib.request.Request(
+        ENDPOINT,
+        data=body,
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+            "Accept": "text/event-stream",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+
+    text_parts = []
+    grounding_chunks = []
+    web_search_queries = []
+
+    try:
+        with urllib.request.urlopen(req, timeout=args.timeout) as response:
+            for data in iter_sse(response):
+                try:
+                    chunk = json.loads(data)
+                except json.JSONDecodeError:
+                    continue
+                for candidate in chunk.get("candidates", []):
+                    content = candidate.get("content", {})
+                    for part in content.get("parts", []):
+                        text = part.get("text")
+                        if text:
+                            text_parts.append(text)
+                    metadata = candidate.get("groundingMetadata")
+                    if metadata:
+                        chunks = metadata.get("groundingChunks")
+                        if isinstance(chunks, list):
+                            grounding_chunks = chunks
+                        queries = metadata.get("webSearchQueries")
+                        if isinstance(queries, list):
+                            web_search_queries = queries
+    except urllib.error.HTTPError as exc:
+        detail = exc.read().decode("utf-8", errors="replace")
+        fail("HTTP %s: %s" % (exc.code, detail))
+    except urllib.error.URLError as exc:
+        fail("Request failed: " + str(exc.reason))
+    except TimeoutError:
+        fail("Request timed out")
+
+    sources = []
+    for gc in grounding_chunks:
+        web = gc.get("web", {})
+        if web.get("uri"):
+            sources.append({"uri": web["uri"], "title": web.get("title", "")})
+
+    print(json.dumps({
+        "status": "succeed",
+        "text": "".join(text_parts),
+        "sources": sources,
+        "webSearchQueries": web_search_queries,
+    }, ensure_ascii=False, separators=(",", ":")))
+
+
+if __name__ == "__main__":
+    main()

.skills/image-to-video/scripts/query_image_to_video.py
@@ -1,0 +1,128 @@
+#!/usr/bin/env python3
+"""
+Poll an already-submitted Kling image-to-video task by task_id.
+
+Use this when generate_image_to_video.py returned {"status":"processing","task_id":"..."}.
+
+Exit codes:
+    0 - success, prints JSON:
+        {"status":"succeed","task_id":"...","videos":[{"url":"...","duration":"...","file":"/path"}]}
+        or, if not finished within the safe time limit:
+        {"status":"processing","task_id":"..."}
+    1 - API or argument error
+"""
+
+import argparse
+import json
+import os
+import sys
+import time
+import urllib.error
+import urllib.parse
+import urllib.request
+
+
+QUERY_URL_BASE = "https://app-dkpgdv8fqio1-api-oYA6Z8wDBRDa.gateway.appmedo.com/v1/videos/image2video"
+POLL_INTERVAL_S = 7
+SAFE_LIMIT_S = 550
+
+
+def fail(message):
+    """Print an error message to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def parse_args():
+    """Parse command line arguments."""
+    p = argparse.ArgumentParser(description="Poll a Kling image-to-video task.")
+    p.add_argument("--task-id", required=True, help="task ID to query")
+    p.add_argument("--output-dir", help="directory to download result videos into")
+    return p.parse_args()
+
+
+def api_key():
+    """Read the API key from the environment; exit if unset."""
+    key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not key:
+        fail("INTEGRATIONS_API_KEY is required")
+    return key
+
+
+def get_json(url, key):
+    """Send a GET request and return the parsed JSON response."""
+    req = urllib.request.Request(
+        url,
+        headers={"X-Gateway-Authorization": "Bearer " + key},
+        method="GET",
+    )
+    try:
+        with urllib.request.urlopen(req, timeout=60) as resp:
+            return json.loads(resp.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        body = e.read().decode("utf-8", errors="replace")
+        raise RuntimeError("HTTP %s: %s" % (e.code, body))
+
+
+def unwrap(data):
+    """Validate the response code and extract the data payload."""
+    if data.get("code") not in (None, 0):
+        raise RuntimeError("API error %s: %s" % (data.get("code"), data.get("message")))
+    return data.get("data", data)
+
+
+def extract_videos(data):
+    """Extract the generated video list from the task result."""
+    result = data.get("task_result") or data.get("taskResult") or {}
+    return result.get("videos") or []
+
+
+def download_videos(videos, output_dir):
+    """Download result videos into the output dir; return URLs only when no dir is given."""
+    if not output_dir:
+        return [{"url": v.get("url"), "duration": v.get("duration"), "file": None} for v in videos]
+    os.makedirs(output_dir, exist_ok=True)
+    downloaded = []
+    for idx, v in enumerate(videos):
+        url = v.get("url")
+        ext = os.path.splitext(urllib.parse.urlparse(url).path)[1] or ".mp4"
+        file_path = os.path.join(output_dir, "video_%s%s" % (idx, ext))
+        urllib.request.urlretrieve(url, file_path)
+        downloaded.append({"url": url, "duration": v.get("duration"), "file": file_path})
+    return downloaded
+
+
+def poll_task(task_id, key, output_dir):
+    """Poll the task status until it succeeds, fails, or the safe limit is reached."""
+    deadline = time.time() + SAFE_LIMIT_S
+    while time.time() < deadline:
+        time.sleep(POLL_INTERVAL_S)
+        data = unwrap(get_json(QUERY_URL_BASE + "/" + urllib.parse.quote(task_id), key))
+        status = data.get("task_status") or data.get("status")
+        if status == "succeed":
+            videos = extract_videos(data)
+            if not videos:
+                raise RuntimeError("succeed response missing videos: %s" % json.dumps(data, ensure_ascii=False))
+            print(json.dumps(
+                {"status": "succeed", "task_id": task_id, "videos": download_videos(videos, output_dir)},
+                ensure_ascii=False,
+            ))
+            return
+        if status in ("failed", "failure"):
+            msg = data.get("task_status_msg") or data.get("message") or "unknown error"
+            raise RuntimeError("Task %s failed: %s" % (task_id, msg))
+    print(json.dumps({"status": "processing", "task_id": task_id}, ensure_ascii=False))
+
+
+def main():
+    """Entry point: poll the image-to-video task and print the result JSON."""
+    args = parse_args()
+    key = api_key()
+    try:
+        poll_task(args.task_id, key, args.output_dir)
+    except Exception as e:
+        fail(str(e))
+
+
+if __name__ == "__main__":
+    main()

.skills/text-to-speech/scripts/call_text_to_speech.py
@@ -1,0 +1,102 @@
+#!/usr/bin/env python3
+"""Call the LemonFox text-to-speech API and save the resulting audio to a file."""
+
+import argparse
+import json
+import os
+import sys
+import urllib.error
+import urllib.request
+
+
+ENDPOINT = "https://app-dkpgdv8fqio1-api-GYX1lzGw01Xa.gateway.appmedo.com/v1/audio/speech"
+
+
+def fail(message):
+    """Print an error to stderr and exit non-zero."""
+    print(message, file=sys.stderr)
+    sys.exit(1)
+
+
+def report_error_body(raw_bytes):
+    """Parse a JSON/text error body and exit with a readable message."""
+    text = raw_bytes.decode("utf-8", errors="replace")
+    try:
+        data = json.loads(text)
+    except ValueError:
+        fail("API error: " + text)
+        return
+    msg = data.get("message") or data.get("error") or json.dumps(data, ensure_ascii=False)
+    fail("API error: " + str(msg))
+
+
+def main():
+    """Entry point: synthesize text to speech and save the audio file."""
+    parser = argparse.ArgumentParser(description="Synthesize text to speech and save the audio file.")
+    parser.add_argument("--input", required=True, help="Text content to convert to speech.")
+    parser.add_argument("--output", required=True, help="Local file path to save the synthesized audio to.")
+    parser.add_argument("--voice", default="heart", help="Voice type, e.g. heart. Default: heart.")
+    parser.add_argument("--response-format", dest="response_format", default="mp3",
+                        help="Output audio format: mp3, wav, ogg. Default: mp3.")
+    parser.add_argument("--timeout", type=int, default=600, help="Request timeout in seconds. Default: 600.")
+    args = parser.parse_args()
+
+    api_key = os.environ.get("INTEGRATIONS_API_KEY")
+    if not api_key:
+        fail("INTEGRATIONS_API_KEY is required")
+
+    body = {
+        "input": args.input,
+        "voice": args.voice,
+        "response_format": args.response_format,
+    }
+    request = urllib.request.Request(
+        ENDPOINT,
+        data=json.dumps(body, ensure_ascii=False).encode("utf-8"),
+        method="POST",
+        headers={
+            "Content-Type": "application/json",
+            "X-Gateway-Authorization": "Bearer " + api_key,
+        },
+    )
+
+    try:
+        with urllib.request.urlopen(request, timeout=args.timeout) as response:
+            content_type = response.headers.get("Content-Type", "")
+            raw = response.read()
+    except urllib.error.HTTPError as exc:
+        raw = exc.read()
+        content_type = exc.headers.get("Content-Type", "") if exc.headers else ""
+        if "json" in content_type or "text" in content_type:
+            report_error_body(raw)
+        else:
+            fail("HTTP %s: %s" % (exc.code, raw.decode("utf-8", errors="replace")))
+        return
+    except urllib.error.URLError as exc:
+        fail("Request failed: " + str(exc.reason))
+        return
+    except TimeoutError:
+        fail("Request timed out")
+        return
+
+    # If the API returned JSON/text, it's an error message wrapped in a normal 200
+    if "json" in content_type or "text" in content_type:
+        report_error_body(raw)
+        return
+
+    try:
+        with open(args.output, "wb") as fh:
+            fh.write(raw)
+    except OSError as exc:
+        fail("Failed to write audio file %s: %s" % (args.output, exc))
+        return
+
+    print(json.dumps(
+        {"status": "succeed", "file": args.output, "bytes": len(raw)},
+        ensure_ascii=False,
+        separators=(",", ":"),
+    ))
+
+
+if __name__ == "__main__":
+    main()